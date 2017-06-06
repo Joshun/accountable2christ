5f344369652351e80ce55b216c659e2e0575a07a
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-06-06 17:34:03 +0100

    separate StruggleList logic completely and tidyup

--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -17,7 +17,6 @@
  * under the License.
  */
 
-var rng;
 
 var app = {
     // Application Constructor
@@ -32,30 +31,19 @@ var app = {
     onDeviceReady: function() {
         console.log("init.");
 
-        rng = new RNG(new Date().getTime());
+        var struggleList = new StruggleList();
+        struggleList.add("Anger Management");
+        struggleList.add("Relationship Issues");
 
-        // $("#test_cell_1").append(createIcon(summariseText("Relationship Issues")));
-        // $("#test_cell_2").append(createIcon(summariseText("Anger Management")));
 
-        var values = [];
-        values.push({icon: createIcon(summariseText("Relationship Issues")), description: "Relationship Issues"});
-        values.push({icon: createIcon(summariseText("Anger Management")), description: "Anger Management"});
-
-        // var icons = [];
-        // icons.push(createIcon(summariseText("Relationship Issues")));
-        // icons.push(createIcon(summariseText("Anger Management")));
-
-        // var descriptions = [ "Relationship Issues", "Anger Management" ];
-
+        var values = struggleList.getList();
+      
         var vm = new Vue({
             el: "#struggles_table_body",
             data: {
                 struggles: values
             }
-            // data: {
-            //     icons: icons,
-            //     descriptions: descriptions
-            // }
+           
         });
 
 
@@ -117,78 +105,3 @@ function showSendStruggleScreen() {
     $("#send_struggle_screen").removeClass("hidden");
 }
 
-function summariseText(text) {
-    if (text == null || text == "") {
-        return null;
-    }
-
-    var words = text.split(" ");
-
-    console.log("words ", words);
-
-    if (words.length == 1) {
-        return words[0][0].toUpperCase();
-    }
-    else {
-        return words[0][0].toUpperCase() + words[words.length-1][0].toUpperCase();
-    }
-}
-
-function createIcon(iconText) {
-    console.log(createIcon);
-    var iconDiv = $("<div>");
-    var iconTextSpan = $("<span>").text(iconText);
-    iconDiv.append(iconTextSpan);
-    iconDiv.addClass("icon");
-    iconDiv.html = iconText;
-
-    // list of [backgroundColor, textColor]
-    var colorPairs = [ ["red", "black"], ["blue", "white"], ["green", "white"], ["orange", "black"], ["pink", "black" ]];
-
-    // https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-    // var randColorIndex = Math.floor(Math.random() * (colors.length-1));
-    var randColorIndex = rng.nextRange(0, colorPairs.length);
-
-    iconDiv.css("background-color", colorPairs[randColorIndex][0]);
-    iconTextSpan.css("color", colorPairs[randColorIndex][1]);
-
-    return iconDiv.prop("outerHTML");
-
-    // $("#test_cell").append(iconDiv);
-
-    // console.log("created icon with iconText", iconText, "and bg color", colors[randColorIndex]);
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Random generator
-// https://stackoverflow.com/questions/424292/seedable-javascript-random-number-generator
-
-function RNG(seed) {
-  // LCG using GCC's constants
-  this.m = 0x80000000; // 2**31;
-  this.a = 1103515245;
-  this.c = 12345;
-
-  this.state = seed ? seed : Math.floor(Math.random() * (this.m-1));
-}
-RNG.prototype.nextInt = function() {
-  this.state = (this.a * this.state + this.c) % this.m;
-  return this.state;
-};
-RNG.prototype.nextFloat = function() {
-  // returns in range [0,1]
-  return this.nextInt() / (this.m - 1);
-};
-RNG.prototype.nextRange = function(start, end) {
-  // returns in range [start, end): including start, excluding end
-  // can't modulu nextInt because of weak randomness in lower bits
-  var rangeSize = end - start;
-  var randomUnder1 = this.nextInt() / this.m;
-  return start + Math.floor(randomUnder1 * rangeSize);
-};
-RNG.prototype.choice = function(array) {
-  return array[this.nextRange(0, array.length)];
-};
--- mobile/www/js/struggle_list.js
+++ mobile/www/js/struggle_list.js
@@ -1,9 +1,48 @@
+var rng = new RNG(new Date().getTime());
 
 function StruggleList(struggles) {
-    this.struggles = (struggles == null) ? [] : struggles;
+    this.struggles = (struggles == null) ? {} : struggles;
 }
 
+StruggleList.prototype.add = function(text) {
+    this.struggles[text] = {
+        icon: createIcon(summariseText(text)),
+        description: text
+    };
+};
+
+StruggleList.prototype.remove = function(text) {
+    if (text in this.struggles) {
+        delete this.struggles[text];
+    }
+};
+
+StruggleList.prototype.getList = function() {
+    var l = [];
+    for (struggle in this.struggles) {
+        l.push(this.struggles[struggle]);
+    }
+    return l;
+};
+
 function createIconText(text) {
+    if (text == null || text == "") {
+        return null;
+    }
+
+    var words = text.split(" ");
+
+    console.log("words ", words);
+
+    if (words.length == 1) {
+        return words[0][0].toUpperCase();
+    }
+    else {
+        return words[0][0].toUpperCase() + words[words.length-1][0].toUpperCase();
+    }
+}
+
+function summariseText(text) {
     if (text == null || text == "") {
         return null;
     }
@@ -38,9 +77,38 @@ function createIcon(iconText) {
     iconDiv.css("background-color", colorPairs[randColorIndex][0]);
     iconTextSpan.css("color", colorPairs[randColorIndex][1]);
 
-    return iconDiv;
+    return iconDiv.prop("outerHTML");
 
     // $("#test_cell").append(iconDiv);
 
     // console.log("created icon with iconText", iconText, "and bg color", colors[randColorIndex]);
 }
+
+
+
+function RNG(seed) {
+  // LCG using GCC's constants
+  this.m = 0x80000000; // 2**31;
+  this.a = 1103515245;
+  this.c = 12345;
+
+  this.state = seed ? seed : Math.floor(Math.random() * (this.m-1));
+}
+RNG.prototype.nextInt = function() {
+  this.state = (this.a * this.state + this.c) % this.m;
+  return this.state;
+};
+RNG.prototype.nextFloat = function() {
+  // returns in range [0,1]
+  return this.nextInt() / (this.m - 1);
+};
+RNG.prototype.nextRange = function(start, end) {
+  // returns in range [start, end): including start, excluding end
+  // can't modulu nextInt because of weak randomness in lower bits
+  var rangeSize = end - start;
+  var randomUnder1 = this.nextInt() / this.m;
+  return start + Math.floor(randomUnder1 * rangeSize);
+};
+RNG.prototype.choice = function(array) {
+  return array[this.nextRange(0, array.length)];
+};
