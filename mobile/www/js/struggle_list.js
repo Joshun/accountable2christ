dcf69229f3c453f856de9fb12ac48ba7f10963ee
Joshua O'Leary <jmoey139@gmail.com>
2017-10-03 15:43:02 +0100

    fix seed changing every time

--- mobile/www/js/struggle_list.js
+++ mobile/www/js/struggle_list.js
@@ -1,10 +1,27 @@
-var rng = new RNG(new Date().getTime());
+var seed;
+
+var loadedSeed = window.localStorage.getItem("icon_color_rng_seed");
+if (loadedSeed == null) {
+    console.log("seed null");
+    seed = new Date().getTime();
+    window.localStorage.setItem("icon_color_rng_seed", seed.toString());
+}
+else {
+    seed = parseInt(loadedSeed);
+    console.log("loaded", seed.toString())
+}
+
+var rng = new RNG(seed);
 
 function StruggleList(vms) {
     this.struggles = {}
     // this.struggles = (struggles == null) ? {} : struggles;
     this.usedColors = [];
     this.vms = vms;
+
+    // this.rng = new RNG(seed);
+    // rng.resetState();
+    // rng = new RNG(seed);
 }
 
 StruggleList.prototype.add = function(name, description) {
@@ -29,7 +46,11 @@ StruggleList.prototype.contains = function(name) {
 }
 
 StruggleList.prototype.clearAll = function() {
+    rng.resetState();
+    // rng = new RNG(seed);
+    
     this.struggles = {};
+    this.usedColors = [];
     for (var i=0; i<this.vms.length; i++) {
         this.vms[i].struggles = this.getList();
     }
@@ -129,12 +150,14 @@ function createIcon(iconText, usedColors) {
 
 
 function RNG(seed) {
+  console.log("create RNG");
   // LCG using GCC's constants
   this.m = 0x80000000; // 2**31;
   this.a = 1103515245;
   this.c = 12345;
 
   this.state = seed ? seed : Math.floor(Math.random() * (this.m-1));
+  this.origState = this.state;
 }
 RNG.prototype.nextInt = function() {
   this.state = (this.a * this.state + this.c) % this.m;
@@ -154,3 +177,6 @@ RNG.prototype.nextRange = function(start, end) {
 RNG.prototype.choice = function(array) {
   return array[this.nextRange(0, array.length)];
 };
+RNG.prototype.resetState = function() {
+    this.state = this.origState;
+}
